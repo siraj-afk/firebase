fbbacb804cf1057280449b74dd1ff79dfa7b1755
siraj-afk <sirajkarimbil805@gmail.com>
2024-11-07 12:43:14 +0530

    kjlk

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pinput/pinput.dart';
 class Otp extends StatefulWidget {
   const Otp({super.key});
 
@@ -9,59 +10,65 @@ class Otp extends StatefulWidget {
 }
 
 class _OtpState extends State<Otp> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
   backgroundColor: Colors.white,
-      body: Padding(
-        padding:  EdgeInsets.only(top: 350.0),
-        child: Column(
-          children: [
-            OtpTextField(
-              numberOfFields: 4,
-              borderColor: Color(0xFF512DA8),
-              //set to true to show as box or false to show as dash
-              showFieldAsBox: true,
-              //runs when a code is typed in
-              onCodeChanged: (String code) {
-                //handle validation or checks here
-              },
-              //runs when every textfield is filled
-              onSubmit: (String verificationCode){
-                showDialog(
-                    context: context,
-                    builder: (context){
-                      return AlertDialog(
-                        title: Text("Verification Code"),
-                        content: Text('Code entered is $verificationCode'),
-                      );
-                    }
-                );
-              }, // end onSubmit
-            ),
-            SizedBox(height: 50.h,),
-            Container(
-              width: 175.w,
-              height: 45.h,
-              decoration: ShapeDecoration(
-                color: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Center(
-                  child: Text(
-                    'send',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w700,
 
+      body: Padding(
+        padding: EdgeInsets.only(top: 400.0.h),
+        child: Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: 385.w,
+                  height: 55.h,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.2199999988079071),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  )),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50.h,),
+
+                SizedBox(height: 50.h,),
+                Container(
+                  width: 175.w,
+                  height: 45.h,
+                  decoration: ShapeDecoration(
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Center(
+                      child: Text(
+                        'push',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w700,
+                          height: 0,
+                        ),
+                      )),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
