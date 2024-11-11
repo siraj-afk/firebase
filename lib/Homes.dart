89e19c1d9b89cdd5281aba5c735303cc717afc4b
siraj-afk <sirajkarimbil805@gmail.com>
2024-11-11 12:16:46 +0530

    popopi

--- lib/Homes.dart
+++ lib/Homes.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class Homes extends StatefulWidget {
   const Homes({super.key});
 
@@ -9,24 +11,70 @@ class Homes extends StatefulWidget {
 }
 
 class _HomesState extends State<Homes> {
+  final ref = FirebaseDatabase.instance.ref("Data");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
       backgroundColor: Colors.white,
-     body: Padding(
-       padding: EdgeInsets.only(left: 20.0.w,top: 50.h),
-       child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-         children: [
-           Expanded(
-             child: ListView.separated(
-                 itemCount: 5,
-                 itemBuilder: (BuildContext context, int index) {
-                   return Text('anything');
-                 }, separatorBuilder: (BuildContext context, int index) { return SizedBox(height: 29.h,); },),
-           ),
-         ],
-       ),
-     ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 20.0.w, top: 50.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                  stream: ref.onValue,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DatabaseEvent> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text('error'),
+                      );
+                    }
+                    if (snapshot.hasData) {
+                      Map<dynamic, dynamic> map =
+                          snapshot.data!.snapshot.value as dynamic;
+
+                      print("hi" + map.values.toString());
+
+                      return ListView.separated(
+                        itemCount: snapshot.data!.snapshot.children.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title:Text(
+                                map.values.toList()[index]["title"].toString()),
+                            trailing: Wrap(
+                              children: [
+                                Icon(Icons.delete),
+                                Icon(Icons.edit),
+                              ],
+                            ),
+
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(
+                            height: 29.h,
+                          );
+                        },
+                      );
+                    } else {
+                      return SizedBox();
+                    }
+                  }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled6/toastmsg.dart';
 
 import 'Homes.dart';
 
@@ -12,19 +16,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final auth = FirebaseAuth.instance;
+  final post = TextEditingController();
+  final database = FirebaseDatabase.instance.ref("Data");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,title: Center(child: Text('HOME')),
-      actions: [
-        Padding(
-          padding:  EdgeInsets.only(right: 20.0.w),
-          child: GestureDetector(onTap: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (_)=>Homes()));
-          },
-              child: Icon(Icons.arrow_forward_ios_outlined)),
-        )
-      ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(child: Text('HOME')),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0.w),
+            child: Icon(Icons.arrow_forward_ios_outlined),
+          )
+        ],
       ),
       backgroundColor: Colors.black,
       body: Padding(
@@ -48,54 +55,59 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   child: TextField(
+                    controller: post,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                     ),
                   ),
                 ),
-                SizedBox(height: 50.h,),
-                Container(
-                  width: 175.w,
-                  height: 45.h,
-                  decoration: ShapeDecoration(
-                    color: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Center(
-                      child: Text(
-                    'push',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w700,
-                      height: 0,
-                    ),
-                  )),
+                SizedBox(
+                  height: 50.h,
                 ),
-                SizedBox(height: 50.h,),
-                Container(
-                  width: 175.w,
-                  height: 45.h,
-                  decoration: ShapeDecoration(
-                    color: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                GestureDetector(
+                  onTap: () {
+                    final id = DateTime.now().microsecondsSinceEpoch.toString();
+                    database
+                        .child(id)
+                        .set({"id": id, "title": post.text.toString()})
+                        .then(
+                          (value) => {
+                            ToastMessage()
+                                .toastmessage(message: 'adding Succesfully'),
+                            post.clear(),
+                            Navigator.of(context).push(
+                                MaterialPageRoute(builder: (_) => Homes()))
+                          },
+                        )
+                        .onError(
+                          (error, stackTrace) => ToastMessage()
+                              .toastmessage(message: error.toString()),
+                        );
+                  },
+                  child: Container(
+                    width: 175.w,
+                    height: 45.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
+                    child: Center(
+                        child: Text(
+                      'push',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    )),
                   ),
-                  child: Center(
-                      child: Text(
-                        'push',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w700,
-                          height: 0,
-                        ),
-                      )),
+                ),
+                SizedBox(
+                  height: 50.h,
                 ),
               ],
             ),
